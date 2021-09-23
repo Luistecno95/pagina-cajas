0261429d54d5343cfbe2f398fe49d64d9c23a105
Luistecno95 <85687121+Luistecno95@users.noreply.github.com>
2021-09-23 00:27:58 -0500

    Add files via upload

---
+++ Act1_programasPrimmitivos/js/funciones.js
@@ -0,0 +1,19 @@
+function factorialFor(){
+    var x = parseInt(document.getElementById("n1").value);
+    var = "";
+    for(i=0; i<x; i++){
+        texto+= "<option value='1'>0</option>";
+        texto+= "<option value='2'>0</option>";
+        texto+= "<option value='3'>0</option>";
+        texto+= "<option value='4'>0</option>";
+        texto+= "<option value='5'>0</option>";
+        texto+= "<option value='6'>0</option>";
+        texto+= "<option value='7'>0</option>";
+        texto+= "<option value='8'>0</option>";
+        texto+= "<option value='9'>0</option>";
+        texto+= "<option value='10'>0</option selected>";
+    }
+    var mensaje = document.getElementById("resultado").innerHTML;
+    alert(mensaje);
+
+}
